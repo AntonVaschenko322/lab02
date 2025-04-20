06fcd86f472a27f38c370d79e70b10062cf47d56
AntonVaschenko322 <antonvaschenko6@gmail.com>
2025-04-20 23:18:55 +0300

    Hello World with comments

--- examples/Hello_world.cpp
+++ examples/Hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 int main(int argc, char** argv)
 {
- std::string name;
+ std::string name; //user name
  std::cout<<"Enter your name \n";
  std::cin>>name;
- std::cout<<"Hello World from "<<name<<std::endl;
+ std::cout<<"Hello World from "<<name<<std::endl; //result
 }
