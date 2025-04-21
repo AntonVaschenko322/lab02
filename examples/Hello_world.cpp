5aa1040a0121620c26f2860c22f80b83c234e7fa
AntonVaschenko322 <antonvaschenko6@gmail.com>
2025-04-21 13:52:25 +0300

    New comments, on Russian now

--- examples/Hello_world.cpp
+++ examples/Hello_world.cpp
@@ -2,8 +2,8 @@
 #include <string>
 int main(int argc, char** argv)
 {
- std::string name; //user name
+ std::string name; //Имя пользователя 
  std::cout<<"Enter your name \n";
  std::cin>>name;
- std::cout<<"Hello World from "<<name<<std::endl; //result
+ std::cout<<"Hello World from "<<name<<std::endl; //Результат
 }
